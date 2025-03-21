0f5a9a196e26457c09dd3d2cf6f8c2bbdd36ec00
Dan Whitman <dwhitman44@gmail.com>
2025-03-21 12:15:07 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 animation_rate = 250
 
-class PlayerData :
-    def __init__(self, x) :
-        self.x = x
+class Character :
+    def __init__(self, down_frames: List[Image], tile_x: int, tile_y: int) :
+        self.sprite = sprites.create(down_frames[0], SpriteKind.Player)
+        scene.camera_follow_sprite(self.sprite)
+        animation.run_image_animation(self.sprite, down_frames, animation_rate, True)
+        self.sprite.set_position((tile_x + 0.5)*16, (tile_y + 0.5)*16)
+        controller.move_sprite(self.sprite, 50, 50)
 
-x = PlayerData(6)
+    def destroy(self) :
+        sprites.destroy(self.sprite)
 
-tiles.set_tilemap(tilemap("alex-level"))
-alex_sprite = sprites.create(assets.animation("alex-down")[0], SpriteKind.Player)
-animation.run_image_animation(alex_sprite, assets.animation("alex-down"), animation_rate, True)
-
-
+character = Character(assets.animation("alex-down"), 15, 15)
 
 scene.set_background_color(7)
-scene.camera_follow_sprite(alex_sprite)
-alex_sprite.set_position(15.5*16, 15.5*16)
-controller.move_sprite(alex_sprite, 50, 50)
+tiles.set_tilemap(tilemap("alex-level"))
+
+def ugh() :
+    character.destroy()
+
+controller.A.on_event(ControllerButtonEvent.PRESSED, ugh)
+
--- main.ts
+++ main.ts
@@ -1,17 +1,23 @@
 let animation_rate = 250
-class PlayerData {
-    x: number
-    constructor(x: number) {
-        this.x = x
+class Character {
+    sprite: Sprite
+    constructor(down_frames: Image[], tile_x: number, tile_y: number) {
+        this.sprite = sprites.create(down_frames[0], SpriteKind.Player)
+        scene.cameraFollowSprite(this.sprite)
+        animation.runImageAnimation(this.sprite, down_frames, animation_rate, true)
+        this.sprite.setPosition((tile_x + 0.5) * 16, (tile_y + 0.5) * 16)
+        controller.moveSprite(this.sprite, 50, 50)
+    }
+    
+    public destroy() {
+        sprites.destroy(this.sprite)
     }
     
 }
 
-let x = new PlayerData(6)
-tiles.setTilemap(tilemap`alex-level`)
-let alex_sprite = sprites.create(assets.animation`alex-down`[0], SpriteKind.Player)
-animation.runImageAnimation(alex_sprite, assets.animation`alex-down`, animation_rate, true)
+let character = new Character(assets.animation`alex-down`, 15, 15)
 scene.setBackgroundColor(7)
-scene.cameraFollowSprite(alex_sprite)
-alex_sprite.setPosition(15.5 * 16, 15.5 * 16)
-controller.moveSprite(alex_sprite, 50, 50)
+tiles.setTilemap(tilemap`alex-level`)
+controller.A.onEvent(ControllerButtonEvent.Pressed, function ugh() {
+    character.destroy()
+})
